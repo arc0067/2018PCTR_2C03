b3810464ac39446d30965588cdf136ad70afb98f
AlissonRomero <arc0067@alu.ubu.es>
2018-06-12 01:46:21 +0200

    Implementación de la clase Jugador
    Se añaden metodos get y set, y se crea un contructor solo con el id

--- ClubDeGolf/src/pl02/Jugador.java
+++ ClubDeGolf/src/pl02/Jugador.java
@@ -6,14 +6,14 @@ import java.util.concurrent.TimeUnit;
 
 public class Jugador implements Runnable {
 	
-	private int id;
+	protected int id;
 	private boolean experto;
 	private int numVueltas;
 	private Club club;
 	private Random rand = new Random();
 	
-	public Jugador() {
-			
+	public Jugador(int id) {
+		this.id=id;
 	}
 	
 	public Jugador(int id, boolean experto, int numVueltas, Club elClub) {
@@ -25,23 +25,56 @@ public class Jugador implements Runnable {
 
 	@Override
 	public void run() {
-		int aleatorio = ThreadLocalRandom.current().nextInt(0,999);
+		int jugar = ThreadLocalRandom.current().nextInt(0,999);
+		int descansar = ThreadLocalRandom.current().nextInt(0,999);
 		while(numVueltas != 0) {
 			try {
 				TimeUnit.MILLISECONDS.sleep(rand.nextInt(1000));
 				//reservar
 				club.reservar(club.getNumPelotasIniciales(),club.getNumPalosIniciales());
 				//jugar
-				TimeUnit.MILLISECONDS.sleep(aleatorio);
+				TimeUnit.MILLISECONDS.sleep(jugar);
 				//devolver
 				club.devolver(club.getNumPelotasIniciales(), club.getNumPalosIniciales());
 				//descansar
-				TimeUnit.MILLISECONDS.sleep(aleatorio);
+				TimeUnit.MILLISECONDS.sleep(descansar);
 			}catch(InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
 
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public boolean isExperto() {
+		return experto;
+	}
+
+	public void setExperto(boolean experto) {
+		this.experto = experto;
+	}
+
+	public int getNumVueltas() {
+		return numVueltas;
+	}
+
+	public void setNumVueltas(int numVueltas) {
+		this.numVueltas = numVueltas;
+	}
+
+	public Club getClub() {
+		return club;
+	}
+
+	public void setClub(Club club) {
+		this.club = club;
 	}
 
 }
