306b506ad49a3ff01afed0964d25edd03c21a0f1
AlissonRomero <arc0067@alu.ubu.es>
2018-06-12 04:37:11 +0200

    Documentación de la clase Jugador

--- ClubDeGolf/src/pl02/Jugador.java
+++ ClubDeGolf/src/pl02/Jugador.java
@@ -4,20 +4,64 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Clase Hilo que implementa la interfaz Runnable.
+ * 
+ * @author Alisson Romero Chila
+ *
+ */
 public class Jugador implements Runnable {
-	
-	protected int id;
+	/**
+	 * Atributo tipo int.
+	 */
+	private int id;
+	/**
+	 * Atributo tipo boolean.
+	 */
 	private boolean experto;
+	/**
+	 * Atributo tipo int.
+	 */
 	private int numVueltas;
+	/**
+	 * Objeto tipo Club.
+	 */
 	private Club club;
+	/**
+	 * Objeto tipo Random.
+	 */
 	private Random rand = new Random();
-	private int jugar = ThreadLocalRandom.current().nextInt(0,999);
-	private int descansar = ThreadLocalRandom.current().nextInt(0,999);
-	
+	/**
+	 * Atributo tipo int.
+	 */
+	private int jugar = ThreadLocalRandom.current().nextInt(0, 999);
+	/**
+	 * Atributo tipo int.
+	 */
+	private int descansar = ThreadLocalRandom.current().nextInt(0, 999);
+
+	/**
+	 * Definicion constructor solo con el id.
+	 * 
+	 * @param id
+	 *            identificador.
+	 */
 	public Jugador(int id) {
-		this.id=id;
+		this.id = id;
 	}
-	
+
+	/**
+	 * Definicion constructor con el identificador y el tipo de jugador.
+	 * 
+	 * @param id
+	 *            id.
+	 * @param experto
+	 *            experto.
+	 * @param numVueltas
+	 *            numvueltas.
+	 * @param elClub
+	 *            elClub.
+	 */
 	public Jugador(int id, boolean experto, int numVueltas, Club elClub) {
 		this.id = id;
 		this.experto = experto;
@@ -27,71 +71,48 @@ public class Jugador implements Runnable {
 
 	@Override
 	public void run() {
-		
-		while(getNumVueltas() <= Simulador.NUM_VUELTAS) {
+
+		while (getNumVueltas() <= Simulador.NUM_VUELTAS) {
 			try {
 				TimeUnit.MILLISECONDS.sleep(rand.nextInt(1000));
-				//reservar
-				club.reservar(club.getNumPelotasIniciales(),club.getNumPalosIniciales());
-				//jugar
+				// reservar
+				club.reservar(club.getNumPelotasIniciales(), club.getNumPalosIniciales());
+				// jugar
 				TimeUnit.MILLISECONDS.sleep(jugar);
-				//devolver
+				// devolver
 				club.devolver(club.getNumPelotasIniciales(), club.getNumPalosIniciales());
-				//descansar
+				// descansar
 				TimeUnit.MILLISECONDS.sleep(descansar);
-			}catch(InterruptedException e) {
+			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
 
 	}
-	
+
+	// Implementacion de getters
 	public int getId() {
 		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
 	}
 
 	public boolean isExperto() {
 		return experto;
 	}
 
-	public void setExperto(boolean experto) {
-		this.experto = experto;
-	}
-
 	public int getNumVueltas() {
 		return numVueltas;
-	}
-
-	public void setNumVueltas(int numVueltas) {
-		this.numVueltas = numVueltas;
 	}
 
 	public Club getClub() {
 		return club;
 	}
 
-	public void setClub(Club club) {
-		this.club = club;
-	}
-	
 	public int getJugar() {
 		return jugar;
 	}
 
-	public void setJugar(int jugar) {
-		this.jugar = jugar;
-	}
-
 	public int getDescansar() {
 		return descansar;
-	}
-
-	public void setDescansar(int descansar) {
-		this.descansar = descansar;
 	}
 
 }
